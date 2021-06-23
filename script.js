763bace88df2f3936df9e511bafb84a15882dfb3
anya lozovaya <anyalozovaya2014@gmail.com>
2021-06-24 00:19:38 +0400

    feat: add slider (desktop)

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
 const prevSlide = document.querySelector('.slider_item_switches__left-arrow')
 const nextSlide = document.querySelector('.slider_item_switches__right-arrow')
-const content = document.querySelector('.slider_item_content')
+const slides = document.querySelector('.slider_item').querySelector('.wrapper').children
+
 const bg = document.querySelector('.slider_item_circle')
 const bgDog = document.querySelector('.slider_item_circle-dog')
 const nextContent = () => {
 
     bg.style.opacity = "0"
     bgDog.style.opacity = "1"
-
-
+    slides[1].style.right = "1440px"
+    slides[2].style.right = "608px"
+    console.log('next');
     prevSlide.removeAttribute('disabled')
+    nextSlide.setAttribute('disabled', 'disabled')
+    nextSlide.classList.toggle('disabled_switch')
+    prevSlide.classList.toggle('disabled_switch')
+    nextSlide.classList.toggle('active_switch')
+    prevSlide.classList.toggle('active_switch')
 }
 
 const prevContent = () => {
 
-    // content.classList = "slider_item_content"
+    console.log('prev');
     bgDog.style.opacity = "0"
     bg.style.opacity = "1"
+    slides[1].style.right = "82px"
+    slides[2].style.right = "-720px"
+    nextSlide.removeAttribute('disabled')
+    prevSlide.setAttribute('disabled', 'disabled')
+    nextSlide.classList.toggle('disabled_switch')
+    prevSlide.classList.toggle('disabled_switch')
+    nextSlide.classList.toggle('active_switch')
+    prevSlide.classList.toggle('active_switch')
 
 }
 
