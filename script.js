3ab1ab9ce99c92378bc6064f867698c2f2dd6b45
anya lozovaya <anyalozovaya2014@gmail.com>
2021-06-24 03:22:48 +0400

    feat: add popup, add actions

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Слайдер
 const prevSlide = document.querySelector('.slider_item_switches__left-arrow')
 const nextSlide = document.querySelector('.slider_item_switches__right-arrow')
 const slides = document.querySelector('.slider_item').querySelector('.wrapper').children
@@ -5,12 +6,10 @@ const slides = document.querySelector('.slider_item').querySelector('.wrapper').
 const bg = document.querySelector('.slider_item_circle')
 const bgDog = document.querySelector('.slider_item_circle-dog')
 const nextContent = () => {
-
     bg.style.opacity = "0"
     bgDog.style.opacity = "1"
     slides[1].style.right = "1440px"
     slides[2].style.right = "608px"
-    console.log('next');
     prevSlide.removeAttribute('disabled')
     nextSlide.setAttribute('disabled', 'disabled')
     nextSlide.classList.toggle('disabled_switch')
@@ -20,8 +19,6 @@ const nextContent = () => {
 }
 
 const prevContent = () => {
-
-    console.log('prev');
     bgDog.style.opacity = "0"
     bg.style.opacity = "1"
     slides[1].style.right = "82px"
@@ -40,9 +37,9 @@ prevSlide.addEventListener('click', prevContent)
 
 
 
-// карточка с формой (openForm)
+// Карточка с формой (openForm)
 const doctorCard = document.querySelector('.content__card-doctor')
-const doctorForm = document.querySelector('.content__card-form')
+const doctorForm = document.querySelector('.content__card-info')
 
 const openForm = () => {
     doctorCard.style.right = "100%"
@@ -53,4 +50,44 @@ const closeForm = () => {
 }
 
 doctorCard.addEventListener('mouseover', openForm)
-doctorForm.addEventListener('mouseout', closeForm)
+doctorForm.addEventListener('mouseleave', closeForm)
+
+// Попап
+
+const popup = document.querySelector('.popup')
+const buttonMore = document.querySelector('.second_slide_information_button')
+const closePopup = document.querySelector('.popup_form_cross-icon')
+const buttonPopup = document.querySelector('.popup_form_button')
+
+let isOpen = false
+let sendIt
+
+const showPopup = (sendIt) => {
+    if (!isOpen) {
+        popup.style.display = "flex"
+        isOpen = true
+    } else if (isOpen && sendIt == undefined) {
+        popup.style.display = "none"
+        isOpen = false
+    } else if (isOpen && sendIt == true) {
+        popup.style.display = "none"
+        isOpen = false
+    }
+}
+
+buttonMore.addEventListener('click', showPopup)
+closePopup.addEventListener('click', showPopup)
+buttonPopup.addEventListener('click', () => {
+    sendIt = true
+    const values = document.querySelectorAll('.popup_form_details_inputs_item')
+    for (let i = 0; i < values.length; i++) {
+        if (values[i].hasAttribute("required") && values[i].value === "") {
+            sendIt = false
+            values[i].style.borderBottom = "1px solid red"
+        } else {
+            values[i].style.borderBottom = "1px solid #394573"
+        }
+    }
+
+    showPopup(sendIt)
+})
